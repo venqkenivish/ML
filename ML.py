dc95f56575e8b5d0912a1c94bbf0d189a58c3ad8
venqkenivish <alikhan.bolat.kz@gmial.com>
2023-03-21 20:12:08 +0600

    -ML system ready parameters:classifier,dense(relu) 50,dense(sigmoid) 1epochs=64

--- ML.py
+++ ML.py
@@ -3,40 +3,63 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.layers import Dense,Flatten
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+from keras.layers import Dense, Dropout, Flatten, Reshape, Conv1D, LSTM, BatchNormalization, Dropout, GRU
+from keras.models import Sequential
+from tensorflow.random import set_seed
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 import random
 data = pd.read_csv("C:/Users/Workstation/git/ML/data/acc-data.csv")
+data.columns
+len(data[data['Classs']==1])/50
 n = 39
-X = np.empty((n,50*3))
+X = np.empty((n,50,3))
 Y = np.empty(n)
 for i in range(0,n):
-    for j in range(0,3):
-        X[i,j*50:(j+1)*50]=data.iloc[i*50:(i+1)*50,j+2]
+    X[i]=data.iloc[i*50:(i+1)*50,2:5]
     Y[i]=data.iloc[i*50,5]
     
 from sklearn.model_selection import train_test_split
 X_train, X_test,Y_train, Y_test = train_test_split(X,Y, test_size=0.3,random_state=8, shuffle=True)
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-lr = keras.Sequential([
-    Flatten(input_shape=(28, 28, 1)),
-    Dense(128, activation='relu'),
-    Dense(10, activation='softmax')
-])
+X_train.shape
 
-print(lr.summary())   
-lr.compile(optimizer='adam',
-             loss='categorical_crossentropy',
-             metrics=['accuracy'])
+def get_model():
+    set_seed(2)
+    model = Sequential([
+        Dense(50, activation='relu',input_shape=(50,3)),        
+        Flatten(),
+        Dense(1, activation='sigmoid')
+    ])
+    
+    model.compile(loss='binary_crossentropy',
+                optimizer=Adam(learning_rate=0.01),metrics=['accuracy',keras.metrics.Precision(),keras.metrics.Recall()])
+    return model
+k=3
+size = X_train.shape[0] // k
+avg_scores = []
+for i in range(k):
+    
+    print("fold : ", i)
+    model = get_model()
+    
+    X_train_ = np.concatenate([X_train[:i*size] , X_train[(i+1)*size:]],axis=0)
+    X_val = X_train[i*size: (i+1)*size]
+    
+    Y_train_ = np.concatenate([Y_train[:i*size] , Y_train[(i+1)*size:]])
+    Y_val = Y_train[i*size: (i+1)*size]
+    
+    model = get_model()
+    
+    model.fit(X_train_,Y_train_,epochs=12)
+    scores = model.evaluate(X_val, Y_val)
+    avg_scores.append(scores)
 
-
-lr.fit(X_train, Y_train, batch_size=32, epochs=5, validation_split=0.2)
-
-lr.evaluate(X_test,Y_test)
-pred_y = lr.predict(X_test)
-print(accuracy_score(Y_test,pred_y))
-print(precision_score(Y_test,pred_y))
-print(recall_score(Y_test,pred_y))
+np.average(avg_scores,axis=0)
+model = get_model()
+model.fit(X_train,Y_train,epochs=64)
+score=model.evaluate(X_test,Y_test)
+print(model.metrics_names[1])
+score[1]
