02ceac30d64b44aa48d0b300bc063380c2ed61f2
venqkenivish <alikhan.bolat.kz@gmial.com>
2023-03-21 17:59:36 +0600

    -ML redacted from 24 to 42 string

--- ML.py
+++ ML.py
@@ -5,23 +5,38 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tensorflow import keras
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense,Flatten
 import random
-"""
 data = pd.read_csv("C:/Users/Workstation/git/ML/data/acc-data.csv")
-data
-data.columns
-"""
-c=np.array([-40,-10,0,8,15,22,38,])
-f=np.array([-40,14,32,46,59,72,100,])
-model = keras.Sequential()
-model.add(Dense(units=1, input_shape=(1,),activation='linear'))
+n = 39
+X = np.empty((n,50*3))
+Y = np.empty(n)
+for i in range(0,n):
+    for j in range(0,3):
+        X[i,j*50:(j+1)*50]=data.iloc[i*50:(i+1)*50,j+2]
+    Y[i]=data.iloc[i*50,5]
+    
+from sklearn.model_selection import train_test_split
+X_train, X_test,Y_train, Y_test = train_test_split(X,Y, test_size=0.3,random_state=8, shuffle=True)
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+lr = keras.Sequential([
+    Flatten(input_shape=(28, 28, 1)),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
 
-model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(0.1))
-history = model.fit(c,f,epochs=500, verbose=0 )
+print(lr.summary())   
+lr.compile(optimizer='adam',
+             loss='categorical_crossentropy',
+             metrics=['accuracy'])
 
-plt.plot(history.history['loss'])
-plt.grid(1)
-plt.show()
-print(model.predict([100]))
-print(model.get_weights())
+
+lr.fit(X_train, Y_train, batch_size=32, epochs=5, validation_split=0.2)
+
+lr.evaluate(X_test,Y_test)
+pred_y = lr.predict(X_test)
+print(accuracy_score(Y_test,pred_y))
+print(precision_score(Y_test,pred_y))
+print(recall_score(Y_test,pred_y))
